8c37cb18dda05a624e734556c2826aac83932e36
Jyrki Pulliainen <jyrki@dywypi.org>
2009-03-23 23:09:47 +0200

    Correct more imports

--- aanikone/votechecker/urls.py
+++ aanikone/votechecker/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns(
-    'votechecker.views',
+    'aanikone.votechecker.views',
     (r'^$', 'index'),
     (r'^whois/$', 'whois'),
     (r'^vote/$', 'vote'),
