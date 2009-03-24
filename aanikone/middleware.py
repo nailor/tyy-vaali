f4664cac4cacdaae0fb2f88e6a0efed08e9875c8
Jyrki Pulliainen <jyrki@dywypi.org>
2009-03-24 21:34:34 +0200

    Authenticate users in shibboleth

---
+++ aanikone/middleware.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponseForbidden
+from django.contrib.auth.models import User
+
+class ShibbolethMiddleware(object):
+    """
+    Middleware that checks that the user authenticated via Shibboleth
+    is the user we actually want.
+    """
+    def process_request(self, request):
+        """Authenticate user against the data from Shibboleth.
+
+        Tse provides uid in the response, Utu provides mail in form of
+        username@utu.fi.
+
+        """
+        try:
+            if 'uid' in request.META:
+                username = request.META['uid']
+            else:
+                username, domain = request.META['mail'].split('@')
+            user = User.objects.get(username=username)
+            request.__class__.user = user
+        except:
+            return HttpResponseForbidden('You fail.')
